2ab9898a290b9bb9e0eb41713d0e77b2b093f16e
Steven Leiva <leiva.steven@gmail.com>
2015-12-16 15:39:06 -0500

    Add ability of to randoly pick a quote from rickygervais.txt.

--- quote_machine.rb
+++ quote_machine.rb
@@ -2,7 +2,17 @@ require 'rack'
 
 class QuoteMachine
   def call(env)
-    [200, {'Content-Type' => 'text/plain'}, ['Hello World']]
+    [200, {'Content-Type' => 'text/plain'}, [quote]]
+  end
+
+  private
+
+  def quote
+    quotes.sample
+  end
+
+  def quotes
+    IO.readlines('./fixtures/rickygervais.txt')
   end
 end
 
