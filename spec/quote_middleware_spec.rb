da48e8c0507e37d6cdda1121568d6201b72e26bb
Steven Leiva <leiva.steven@gmail.com>
2015-12-16 23:37:50 -0500

    Add setup to our test suite.
    We have added a few setup items to our test suite. In particular, we've
    created a simple app that we can instantiate our middleware with.
    
    We've created an instance of our Middleware with our simple app, so that
    it is ready for testing.
    
    We've created a method called stubbed_quotes that will reach into the
    Quotes module and pull out the array of quotes defined there.
    
    We've created a request method that uses an instance of the
    Rack::MockRequest object instantiated with our middleware to avoid
    making actual HTTP calls.
    
    Finally, note that we are using allow_any_instance_of to stub out the
    call to QuoteMiddleware#quotes. Normally, we'd use
    allow(QuoteMiddleware) instead, but this does not work for private
    methods such as QuoteMiddleware#quotes.

--- spec/quote_middleware_spec.rb
+++ spec/quote_middleware_spec.rb
@@ -1,6 +1,21 @@
 require_relative '../lib/quote_middleware'
+require_relative 'stubs/quotes'
 
 RSpec.describe QuoteMiddleware do
+  let(:app) do
+    # simple object that responds to #call
+    -> env { [200, {'Content-Text' => 'text/plain'}, ['Underlying App Executed']] }
+  end
+
+  let(:middleware)     { QuoteMiddleware.new(app) }
+  let(:request)        { Rack::MockRequest.new(middleware) }
+  let(:stubbed_quotes) { Quotes.quotes }
+
+  before(:each) do
+    # must use allow_any_instance_of because quotes is a private method
+    allow_any_instance_of(QuoteMiddleware).to receive(:quotes).and_return(stubbed_quotes)
+  end
+
   it "passes a test" do
     expect("true").to eq("true")
   end
